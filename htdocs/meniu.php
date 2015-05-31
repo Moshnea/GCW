4271854d2210ecb03efff8e822aebc040d762551
Moshnea <ronaldo07min@gmail.com>
2015-05-31 15:57:48 +0300

    Update Menu

--- htdocs/meniu.php
+++ htdocs/meniu.php
@@ -20,73 +20,34 @@
 
 	<?php require("pop-up.php"); ?>
 
-	</header>
-
 	<div id = "img_meniu"></div>
 
-	<!--
-	<section id = "listameniustg">
-	<ul style = "list-style-type:none;">
-
-		<li>
-			<h2>Apple Juice</h2>
-			<h3>Cheese, tomato, mushrooms, onions.</h3>
-		</li>
-
-		<li>
-			<h2>Berry Juice </h2>
-			<h3>Chicken, mozzarella cheese, onions.</h3>
-		</li>
-
-
-		<li>
-			<h2>Buble Juice</h2>
-			<h3>Tuna, Sweetcorn, Cheese.</h3>
-		</li>
-
-		<li>
-			<h2>BlaBla Juice</h2>
-			<h3>Pineapple, Minced Beef, Cheese.</h3>
-		</li>
-
-	</ul>
-	</section>
-	-->
-<!--
-	<table style = "width: 100%">
-		<tr>
-			<td width = "20%">
-				<img src="img/slider/img1.png" id = "img_produs">
-			</td>
-			<td width = "60%">
-				Denumire: 10 $
-				<br/>
-				Stoc: 10
-				<br/>
-				Pret: 10 $
-			</td>
-			<td width = "20%">
-				<img src="img/slider/img1.png" id = "img_produs">
-			</td>
-		</tr>
-	</table>
--->
 	<section id = "produs">
+		<img src = "img/meniu_bar.png" id = "meniu_bar">
+
 		<div id = "imagine_produs">
 			<img src="img/slider/img1.png" id = "img_produs">
 		</div>
 		<div id="descriere_produs">
-		<br/><br/>
-			Denumire: 10 $
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Denumire: Grande Juicy
 			<br/>
-			Stoc: 10
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Aroma: Banane cu Pamant
+			<br/>
+			&nbsp;&nbsp;&nbsp;&nbsp;Stoc: 10
 			<br/>
 			Pret: 10 $
+			<br/>
+
 		</div>
 		<div id = "imagine_produs">
-			<img src="img/slider/img1.png" id = "img_produs">
+			<a href = "">
+				<img src="img/add_to_cart.png" id = "add_to_cart">
+			</a>
 		</div>
+
+		<img src = "img/meniu_bar.png" id = "meniu_bar">
 	</section>
+
 	<?php require("the_footer.php"); ?>
 
 </body>
